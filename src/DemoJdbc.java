ddae76e2c0dcad416478715a27f4251a83b6aac1
Hariharan <hariharan9572@gmail.com>
2026-01-04 06:02:10 +0530

    Fetching all Records

--- src/DemoJdbc.java
+++ src/DemoJdbc.java
@@ -16,7 +16,7 @@ public class DemoJdbc {
         String url = "jdbc:postgresql://localhost:5432/demo";
         String uname = "postgres";
         String pass = "0000";
-        String sql = "select sname from student where sid = 1";
+        String sql = "select * from student";
 
 //        Class.forName("org.postgresql.Driver"); // optional
         Connection con = DriverManager.getConnection(url, uname, pass);
@@ -24,9 +24,16 @@ public class DemoJdbc {
         Statement st = con.createStatement();
         ResultSet rs = st.executeQuery(sql);
 //        System.out.println(rs.next());
-        rs.next();
-        String name = rs.getString("sname");
-        System.out.println("Name of a student is " + name);
+
+//        rs.next();
+//        String name = rs.getString("sname");
+//        System.out.println("Name of a student is " + name);
+
+        while (rs.next()){
+            System.out.print(rs.getInt(1) + " - ");
+            System.out.print(rs.getString(2) + " - ");
+            System.out.println(rs.getInt(3));
+        }
 
         con.close();
         System.out.println("Connection Closed");
