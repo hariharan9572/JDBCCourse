f3a3fcb56c4767f845bf1cf25d51268e59022183
Hariharan <hariharan9572@gmail.com>
2026-01-04 06:28:30 +0530

    Problem with statement

--- src/DemoJdbc.java
+++ src/DemoJdbc.java
@@ -13,10 +13,14 @@ public class DemoJdbc {
             close
         */
 
+        int sid = 101;
+        String sname = "Anbazhagan";
+        int marks = 65;
+
         String url = "jdbc:postgresql://localhost:5432/demo";
         String uname = "postgres";
         String pass = "0000";
-        String sql = "delete from student where sid = 5";
+        String sql = "insert into student values (" + sid + ", '" + sname + "', " + marks +")";
 
 //        Class.forName("org.postgresql.Driver"); // optional
         Connection con = DriverManager.getConnection(url, uname, pass);
